6c65798521d43ba643ddf86d75d5be9e4cbd22c2
shaadsajjad <sajjadshaad4@mail.com>
2025-03-22 02:51:51 +0600

    dashboard progress

--- lib/Screen/dashboard.dart
+++ lib/Screen/dashboard.dart
@@ -11,6 +11,97 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+       backgroundColor: Colors.green[400],
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: Image.asset("assets/images/logo.png",
+          width: 25,
+            height: 25,
+          ),
+        ),
+        title: Column(
+          children: [
+            Text("Welcome",style: TextStyle(
+              fontSize: 20
+            ),),
+            Text("Shaad",style: TextStyle(
+              fontSize: 16
+            ),)
+          ],
+
+        ),
+        centerTitle: true,
+        
+
+      ),
+      body: Container(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage("assets/images/bg_image.png"),fit: BoxFit.cover)
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            Padding(
+              padding: const EdgeInsets.only(top: 25),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Container(
+
+                      height: 200,
+                      width: 180,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white
+                      ),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/images/medicine.png",
+                          width: 150,
+                          height: 150,),
+                          Text("My Medicine",
+                          style: TextStyle(
+                            fontSize: 24
+                          ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+
+              Container(
+
+                height: 200,
+                width: 180,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white
+                ),
+                child: Column(
+                  children: [
+                    Image.asset("assets/images/reminder.png",
+                      width: 150,
+                      height: 150,),
+                    Text("My Medicine",
+                      style: TextStyle(
+                          fontSize: 24
+                      ),
+                    )
+                  ],
+                ),
+              )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
